e1dd73661f67228af160a386ffbc44b7792e2e45
VaudKK <vaudkeith@gmail.com>
2025-03-11 13:07:08 +0300

    create snippet using form

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"unicode/utf8"
 
 	"net/http"
 	"strconv"
@@ -11,10 +13,11 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
+	// this is now handled by Pat
+	// if r.URL.Path != "/" {
+	// 	app.notFound(w)
+	// 	return
+	// }
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -29,7 +32,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// Pat doesnt strip the colon from the named capture key hence the full colon
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -51,16 +55,50 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request){
+	app.render(w,r,"create.page.tmpl",nil)
+}
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)
+	// if r.Method != "POST" {
+	// 	w.Header().Set("Allow", "POST")
+	// 	app.clientError(w, http.StatusMethodNotAllowed)
+	// 	return
+	// }
+
+	r.Body = http.MaxBytesReader(w,r.Body,4096)
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w,http.StatusBadRequest)
 		return
 	}
 
-	title := "O Snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
-	expires := "7"
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
+
+	errors := make(map[string]string)
+
+	if strings.TrimSpace(title) == ""{
+		errors["title"] = "This field cannot be blank"
+	}else if utf8.RuneCountInString(title) > 100 { // utf8.RuneCount counts the number of chars and not bytes
+		errors["title"] = "This field is too long (maximum is 100 characters)"
+	}
+
+	if strings.TrimSpace(content) == ""{
+		errors["content"] = "This field cannot be blank"
+	}
+
+	if strings.TrimSpace(expires) == "" {
+		errors["expire"] = "This field cannot be blank"
+	}else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "This field is invalid"
+	}
+
+	if len(errors) > 0 {
+		fmt.Fprint(w,errors)
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 
@@ -69,5 +107,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
--- snippetbox/cmd/web/middleware.go
+++ snippetbox/cmd/web/middleware.go
@@ -1,11 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
+
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s",r.RemoteAddr,r.Proto,r.Method,r.URL)
+
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// create a deferred func that will always be called in case of a panic
+		defer func ()  {
+			if err := recover(); err != nil {
+				// set a connection close header on the response
+				w.Header().Set("Connection", "close")
+				app.serverError(w,fmt.Errorf("%s",err))
+			}
+		}()
 
 		next.ServeHTTP(w,r)
 	})
